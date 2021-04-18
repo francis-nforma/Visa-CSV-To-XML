72d1fef7d5af0f8fa08c23c020ec61a1baa60668
francis-nforma <69452109+francis-nforma@users.noreply.github.com>
2021-04-18 17:38:14 +1000

    Add files via upload

---
+++ CreateXMLFile/IResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConvertCSVToXML
+{
+    interface IResult
+    {
+        bool isSuccessful { get; }
+        void addFailure(string field, string failuremessage);
+        void addWarning(string field, string warningmessage);
+        int FailureCount { get; }
+        string getFailureMessage(int index);
+        string getField(int index);
+        void addResultMessage(string logMessage);
+        string ResultMessage { get; }
+        void displayMessage();
+    }
+}
